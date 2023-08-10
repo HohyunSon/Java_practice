4885d434b02e8d1f960acfb4302b0b4185cdc67b
HohyunSon <tpfkajsl98@naver.com>
2023-08-10 15:32:52 +0900

    interface

---
+++ src/InterfacePrac/InterfaceEX2.java
@@ -0,0 +1,42 @@
+package InterfacePrac;
+
+interface Readable
+{
+    void read();
+}
+
+class OuterClass
+{
+    private String myName;
+
+    OuterClass(String name)
+    {
+        myName=name;
+    }
+
+    public Readable createLocalClassInst(final int instID)
+    {
+        return new Readable()
+        {
+            public void read()
+            {
+                System.out.println("Outer inst name: "+myName);
+                System.out.println("Local inst ID: "+instID);
+            }
+        };
+
+    }
+}
+
+class LocalParamClassTest
+{
+    public static void main(String[] args)
+    {
+        OuterClass out=new OuterClass("My Outer Class");
+        Readable localInst1=out.createLocalClassInst(111);
+        localInst1.read();
+
+        Readable localInst2=out.createLocalClassInst(222);
+        localInst2.read();
+    }
+}
