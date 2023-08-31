dc857ce6ca3dfd830d24517f85b37cd12109f065
HohyunSon <tpfkajsl98@naver.com>
2023-08-31 14:35:06 +0900

    FileI/O add

---
+++ src/IOStream/FileMkdir.java
@@ -0,0 +1,20 @@
+package IOStream;
+
+import java.io.File;
+
+class FileMkdir {
+
+	public static void main(String[] args) {
+		File myDir = new File("C:\\YourJava\\JavaDir");	// 디렉터리 위치 정보
+		if(myDir.mkdirs())	// 디렉터리 생성 mkdir과 mkdirs 차이
+			System.out.println("디렉토리 생성 성공");
+		else
+			System.out.println("디렉토리 생성 실패");
+	}
+
+}
+
+/*
+mkdir 디렉토리 하나만 만든다
+mkdirs 하위 디렉토리까지 만든다.
+*/
---
+++ src/IOStream/FileTest3.java
@@ -0,0 +1,17 @@
+package IOStream;
+import java.io.File;
+
+class FileTest3 {
+
+	public static void main(String[] args) {
+		File file = new File("AAA");
+		System.out.println(file.getAbsolutePath());
+		File file2 = new File(file, "BBB");
+		System.out.println(file2.getAbsolutePath());
+	}
+
+}
+
+
+//57. AAA 라는 폴더를 경로로 잡고 절대경로를 출력하자
+//AAA라는 폴더 밑에 BBB라는 폴더를 잡고 그것의 절대경로를 출력하자.
